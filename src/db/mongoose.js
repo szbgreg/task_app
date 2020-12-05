4a3773d11eb4117e00a5246eda88645f5cb25587
szbgreg <szbgreg@gmail.com>
2020-12-05 14:41:59 +0100

    Creating a Mongoose Model

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -12,3 +12,42 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
   console.log('Database connected');
 });
+
+const User = mongoose.model('User', {
+  name: {
+    type: String
+  },
+  age: {
+    type: Number
+  }
+});
+
+const Task = mongoose.model('Task', {
+  description: {
+    type: String
+  },
+  completed: {
+    type: Boolean
+  }
+});
+
+const me = new User({
+  name: 'Greg',
+  age: 33
+});
+
+me.save()
+  .then(() => {
+    console.log(me);
+  })
+  .catch((err) => console.log(err));
+
+const newTask = new Task({
+  description: 'Phone to the doctor',
+  completed: false
+});
+
+newTask
+  .save()
+  .then((data) => console.log(data))
+  .catch((err) => console.log('Error:', err));
