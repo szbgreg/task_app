f66cd1a70ee24e310dea747d83536eeee6c2abe2
szbgreg <szbgreg@gmail.com>
2020-12-06 13:56:56 +0100

    REST API end-points for reading  resources

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 require('./db/mongoose');
 const User = require('./models/user');
 const Task = require('./models/task');
+const { find } = require('./models/user');
 
 const app = express();
 const port = process.env.port || 3000;
@@ -12,6 +13,26 @@ app.get('/', (req, res) => {
   res.send('HomePage');
 });
 
+app.get('/users', (req, res) => {
+  User.find({})
+    .then((users) => {
+      res.send(users);
+    })
+    .catch((e) => res.status(500).send());
+});
+
+app.get('/users/:id', (req, res) => {
+  const _id = req.params.id;
+  User.findById(_id)
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send();
+      }
+      res.send(user);
+    })
+    .catch((err) => res.status(500).send());
+});
+
 app.post('/users', (req, res) => {
   const user = new User(req.body);
   user
@@ -20,6 +41,29 @@ app.post('/users', (req, res) => {
       res.status(201).send(data);
     })
     .catch((err) => res.status(400).send(err));
+});
+
+app.get('/tasks', (req, res) => {
+  Task.find({})
+    .then((tasks) => {
+      res.send(tasks);
+    })
+    .catch((err) => res.status(500).send(err));
+});
+
+app.get('/tasks/:id', (req, res) => {
+  const _id = req.params.id;
+
+  Task.findById(_id)
+    .then((task) => {
+      if (!task) {
+        return res.status(404).send();
+      }
+      res.send(task);
+    })
+    .catch((err) => {
+      res.status(500).send();
+    });
 });
 
 app.post('/tasks', (req, res) => {
