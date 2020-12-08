c7049722740267f67336dae9f35ceb9ce58d84c5
szbgreg <szbgreg@gmail.com>
2020-12-08 18:47:01 +0100

    Filtering tasks data

--- src/routers/task.js
+++ src/routers/task.js
@@ -4,8 +4,13 @@ const auth = require('../midlleware/auth');
 const router = new express.Router();
 
 router.get('/', auth, async (req, res) => {
+  const match = {};
+  if (req.query.completed) {
+    match.completed = req.query.completed === 'true';
+  }
+
   try {
-    await req.user.populate('tasks').execPopulate();
+    await req.user.populate({ path: 'tasks', match }).execPopulate();
     res.send(req.user.tasks);
   } catch (err) {
     res.status(500).send(err);
