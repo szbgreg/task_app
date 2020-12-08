3a722d744b2bbc44614b23aef87c9929040d8ae5
szbgreg <szbgreg@gmail.com>
2020-12-08 19:05:13 +0100

    Sorting tasks data

--- src/routers/task.js
+++ src/routers/task.js
@@ -5,8 +5,15 @@ const router = new express.Router();
 
 router.get('/', auth, async (req, res) => {
   const match = {};
+  const sort = {};
+
   if (req.query.completed) {
     match.completed = req.query.completed === 'true';
+  }
+
+  if (req.query.sortBy) {
+    const parts = req.query.sortBy.split(':');
+    sort[parts[0]] = parts[1] === 'desc' ? -1 : 1;
   }
 
   try {
@@ -16,7 +23,8 @@ router.get('/', auth, async (req, res) => {
         match,
         options: {
           limit: parseInt(req.query.limit),
-          skip: parseInt(req.query.skip)
+          skip: parseInt(req.query.skip),
+          sort
         }
       })
       .execPopulate();
