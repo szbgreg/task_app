8540714e1af21bea8107f8ca990e4030a341cbd4
szbgreg <szbgreg@gmail.com>
2020-12-10 11:51:33 +0100

    Environtment variables

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/task_app', {
+mongoose.connect(process.env.MONGODB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
