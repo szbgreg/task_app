ca8baec465190b5e2e1af9aea91dfbb3bfe697c4
szbgreg <szbgreg@gmail.com>
2020-12-10 12:41:48 +0100

    Testing user signup & login

---
+++ tests/user.test.js
@@ -0,0 +1,55 @@
+const request = require('supertest');
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+const app = require('../src/app');
+const User = require('../src/models/user');
+
+const userOneId = mongoose.Types.ObjectId();
+
+const userOne = {
+  _id: userOneId,
+  name: 'Mike',
+  email: 'mike@test.hu',
+  password: 'mikepass123',
+  tokens: [
+    {
+      token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET)
+    }
+  ]
+};
+
+beforeEach(async () => {
+  await User.deleteMany();
+  await new User(userOne).save();
+});
+
+test('Should signup a new user', async () => {
+  await request(app)
+    .post('/users')
+    .send({
+      name: 'jesttest',
+      email: 'jest@test.hu',
+      password: 'mypasswo234'
+    })
+    .expect(201);
+});
+
+test('Should login existing user', async () => {
+  await request(app)
+    .post('/users/login')
+    .send({
+      email: userOne.email,
+      password: userOne.password
+    })
+    .expect(200);
+});
+
+test('Should not login with non existing user', async () => {
+  await request(app)
+    .post('/users/login')
+    .send({
+      email: userOne.email,
+      password: 'adsfasdf23'
+    })
+    .expect(400);
+});
